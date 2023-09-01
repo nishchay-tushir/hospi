f49daa56788ff0aff9ac3dfe85c0bc2a94380897
nishchay-tushir <instatushir@gmail.com>
2023-09-01 13:06:04 +0530

    first

---
+++ yadav/home.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loadingScreen = document.getElementById("loadingScreen");
+  
+    // Simulate a delay to demonstrate the effect (replace with your actual content loading logic)
+    setTimeout(function () {
+        loadingScreen.style.opacity = "0";
+        loadingScreen.style.transform = "scale(8)";
+        setTimeout(function () {
+            loadingScreen.style.display = "none";
+            document.body.style.overflow = "auto"; // Enable scrolling after loading screen disappears
+        }, 1500);
+    }, 800); // Adjust the timeout (in milliseconds) to match your desired delay
+    });
+
+
+   
+
+      
+
+
